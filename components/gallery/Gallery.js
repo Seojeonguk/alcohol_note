9bc1ac2d0dd9229486ac220abf7727a3f4855331
Seojeonguk <uk7880@naver.com>
2022-09-24 23:36:16 +0900

    Provide gallery header
    This will provide a header showing the title.
    
    Signed-off-by: Seojeonguk <uk7880@naver.com>

--- components/gallery/Gallery.js
+++ components/gallery/Gallery.js
@@ -1,5 +1,13 @@
+import { Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { styles } from './styles/GalleryStyles';
 
 export default function Gallery() {
-  return <SafeAreaView></SafeAreaView>;
+  return (
+    <SafeAreaView>
+      <View style={styles.galleryHeader}>
+        <Text style={styles.galleryTitle}>Gallery</Text>
+      </View>
+    </SafeAreaView>
+  );
 }
---
+++ components/gallery/styles/GalleryStyles.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  galleryHeader: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderBottomWidth: 1,
+    borderStyle: 'solid',
+    borderColor: 'grey',
+    paddingVertical: 5,
+  },
+  galleryTitle: {
+    textAlign: 'center',
+    fontSize: 28,
+    fontWeight: 'bold',
+  },
+});
