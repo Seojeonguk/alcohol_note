ece4a393119f45de98a5e92cf6fbdc96734b8a84
Seojeonguk <uk7880@naver.com>
2022-09-24 23:43:10 +0900

    Add dummy data
    This will provide dummy data.
    
    Signed-off-by: Seojeonguk <uk7880@naver.com>

---
+++ components/gallery/dummyData.js
@@ -0,0 +1,73 @@
+export const data = [
+  {
+    no: '1',
+    title: '첫번재 와인',
+    location: '광주 광역시 어딘가',
+    photos: [
+      'https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMjA4MTRfMTc0%2FMDAxNjYwNDI1NTU0MzI0.vESGAzp_-Jq_BwVm33QdW2hGJfWFNvQ8rTFZgNeEJPsg.TUuE85W3vxV38e9WhwQcTfCVytYyKmTbqL3DkUwuTr4g.JPEG.hdwinehouse%2FIMG_4158.jpg&type=sc960_832',
+    ],
+
+    date: '2010-09-19',
+    tags: ['wine', 'good'],
+  },
+  {
+    no: '2',
+    title: '두번째 와인',
+    location: '서울 어딘가',
+    photos: [
+      'https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMjA5MDZfMjUx%2FMDAxNjYyNDM3ODk4NTQ3.FDaxfffFN1iNSBH_t8yTmgw3l30r-1OZIBSyhESQLtYg.wvXYUDZtmsdL1of59htugexzGLe9tBd5VLuqjZqQ9mog.JPEG.janet_hwang%2FIMG_1365.jpg&type=a340',
+    ],
+    date: '2010-12-25',
+    tags: ['wine', 'again'],
+  },
+  {
+    no: '3',
+    title: '첫번째 칵테일',
+    location: '부산 어딘가',
+    photos: [
+      'https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMjA4MjVfMzcg%2FMDAxNjYxNDM1OTY5NDIx.lIbQ_-B4_89VbromG03K3PWHrPuz3X3ZAMIsvHXrs-Ug.8IKBZhhxNBkNsrxPRSlLN5AFi9tDRpLUDut5wIcQgOkg.JPEG.cmwmzl012%2FIMG_1878.JPEG&type=ofullfill340_600_png',
+    ],
+    date: '2010-11-25',
+    tags: ['cocktail', 'tasty'],
+  },
+  {
+    no: '4',
+    title: '두번째 칵테일',
+    location: '울산 어딘가',
+    photos: [
+      'https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMjA3MjdfMjc4%2FMDAxNjU4ODU5MjExNzc2.q7L5K-dJkDVvZU2o3U3CEsHGBWXtBvLdTaVeTP6wG5sg.0XDHIhJN35o6tnemnojnw_j6NWS1L4Lzdju-JVp18QEg.JPEG.happyhope26%2FKakaoTalk_20220727_023445923_07.jpg&type=a340',
+    ],
+    date: '2010-12-26',
+    tags: ['cocktail', 'good'],
+  },
+  {
+    no: '5',
+    title: '3번째 칵테일',
+    location: '광산 어딘가',
+    photos: [
+      'https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMjA3MjdfMjc4%2FMDAxNjU4ODU5MjExNzc2.q7L5K-dJkDVvZU2o3U3CEsHGBWXtBvLdTaVeTP6wG5sg.0XDHIhJN35o6tnemnojnw_j6NWS1L4Lzdju-JVp18QEg.JPEG.happyhope26%2FKakaoTalk_20220727_023445923_07.jpg&type=a340',
+    ],
+    date: '2010-12-26',
+    tags: ['cocktail', 'good'],
+  },
+  {
+    no: '6',
+    title: '4번째 칵테일',
+    location: '울산 어딘가',
+    photos: [
+      'https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMjA3MjdfMjc4%2FMDAxNjU4ODU5MjExNzc2.q7L5K-dJkDVvZU2o3U3CEsHGBWXtBvLdTaVeTP6wG5sg.0XDHIhJN35o6tnemnojnw_j6NWS1L4Lzdju-JVp18QEg.JPEG.happyhope26%2FKakaoTalk_20220727_023445923_07.jpg&type=a340',
+    ],
+    date: '2010-12-26',
+    tags: ['cocktail', 'good'],
+  },
+  {
+    no: '7',
+    title: '두번째 칵테일',
+    location: '울산 어딘가',
+    photos: [
+      'https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMjA3MjdfMjc4%2FMDAxNjU4ODU5MjExNzc2.q7L5K-dJkDVvZU2o3U3CEsHGBWXtBvLdTaVeTP6wG5sg.0XDHIhJN35o6tnemnojnw_j6NWS1L4Lzdju-JVp18QEg.JPEG.happyhope26%2FKakaoTalk_20220727_023445923_07.jpg&type=a340',
+    ],
+    date: '2010-12-26',
+    tags: ['cocktail', 'good'],
+  },
+];
