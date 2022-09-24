30714740e1b8db926737014478e9079d41ebb254
Seojeonguk <uk7880@naver.com>
2022-09-24 22:39:24 +0900

    Add icons
    This will provide an icon for the bottom menu.
    
    Signed-off-by: Seojeonguk <uk7880@naver.com>

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import Gallery from './components/gallery/Gallery';
 import User from './components/user/User';
+import { Entypo, FontAwesome } from '@expo/vector-icons';
 
 const Tab = createMaterialBottomTabNavigator();
 const GalleryStack = createNativeStackNavigator();
@@ -30,8 +31,24 @@ export default function App() {
     <SafeAreaProvider>
       <NavigationContainer>
         <Tab.Navigator initialRouteName="Gallery" activeColor="#fdfffd" inactiveColor="#a3c2ff" shifting>
-          <Tab.Screen name="Gallery" component={GalleryScreen} />
-          <Tab.Screen name="User" component={UserScreen} />
+          <Tab.Screen
+            name="Gallery"
+            component={GalleryScreen}
+            options={{
+              tabBarLabel: 'Gallery',
+              tabBarIcon: ({ color }) => <Entypo name="drink" size={24} color={color} />,
+              tabBarColor: '#eeb7b4',
+            }}
+          />
+          <Tab.Screen
+            name="User"
+            component={UserScreen}
+            options={{
+              tabBarLabel: 'User',
+              tabBarIcon: ({ color }) => <FontAwesome name="user" size={24} color={color} />,
+              tabBarColor: '#f2cfa5',
+            }}
+          />
         </Tab.Navigator>
       </NavigationContainer>
     </SafeAreaProvider>
