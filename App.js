d7c9979be1838247b60149f2a4b3bd9383899ec8
Seojeonguk <uk7880@naver.com>
2022-09-24 22:32:52 +0900

    Add bottom menu
    This will provide a bottom menu that shows the screens of Gallery and User.
    
    Signed-off-by: Seojeonguk <uk7880@naver.com>

--- App.js
+++ App.js
@@ -1,5 +1,20 @@
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import Gallery from './components/gallery/Gallery';
+import User from './components/user/User';
+
+const Tab = createMaterialBottomTabNavigator();
 
 export default function App() {
-  return <SafeAreaProvider></SafeAreaProvider>;
+  return (
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Tab.Navigator initialRouteName="Gallery" activeColor="#fdfffd" inactiveColor="#a3c2ff" shifting>
+          <Tab.Screen name="Gallery" component={Gallery} />
+          <Tab.Screen name="User" component={User} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
+  );
 }
