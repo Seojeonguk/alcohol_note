316234d932f5a02b4642bcd3b87f9ca8e4306d4e
Seojeonguk <uk7880@naver.com>
2022-09-25 22:35:29 +0900

    Change a different style for adding a MasonryList
    This will be changed accordingly with the addition of the MasonryList.
    
    Signed-off-by: Seojeonguk <uk7880@naver.com>

--- components/gallery/GalleryList.js
+++ components/gallery/GalleryList.js
@@ -1,9 +1,18 @@
 import { Image, Text, TouchableOpacity, View } from 'react-native';
 import { styles } from './styles/GalleryListStyles';
+import { useEffect, useState } from 'react';
+
+const h = Math.floor(Math.random() * 100 + 100);
 
 export default function GalleryList({ item }) {
+  const [height, setHeight] = useState(0);
+
+  useEffect(() => {
+    const h = Math.floor(Math.random() * 100 + 200);
+    setHeight(h);
+  }, []);
   return (
-    <View style={styles.itemContainer}>
+    <View style={{ height: height, padding: 5, margin: 5, backgroundColor: 'green' }}>
       <TouchableOpacity style={styles.button} activeOpacity={0.2}>
         <Image source={{ uri: item.photos[0] }} style={styles.itemImage} />
         <View style={styles.itemInfo}>
--- components/gallery/styles/GalleryListStyles.js
+++ components/gallery/styles/GalleryListStyles.js
@@ -1,32 +1,14 @@
 import { StyleSheet } from 'react-native';
+
 export const styles = StyleSheet.create({
-  itemContainer: {
-    flexDirection: 'row',
-    marginVertical: 5,
-    marginHorizontal: 5,
-    padding: 5,
-    flex: 1,
-    backgroundColor: 'green',
-  },
-  button: {
-    alignItems: 'center',
-    padding: 10,
-    backgroundColor: 'tomato',
-    height: '100%',
-    width: '100%',
-  },
+  itemContainer: {},
+  button: {},
   itemImage: {
-    height: 100,
-    width: 100,
-    resizeMode: 'cover',
+    height: '90%',
+    width: '90%',
+    resizeMode: 'stretch',
   },
   itemInfo: {},
-  itemTitle: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  itemDate: {
-    fontSize: 12,
-    color: 'grey',
-  },
+  itemTitle: {},
+  itemDate: {},
 });
