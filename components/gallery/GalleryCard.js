a1fa4972915fd86c17c36bb1796f60f1bc6d09da
Seojeonguk <uk7880@naver.com>
2022-09-26 22:03:41 +0900

    Rename Style Components
    This will also change the style name as the Card component changes.
    
    Signed-off-by: Seojeonguk <uk7880@naver.com>

--- components/gallery/GalleryCard.js
+++ components/gallery/GalleryCard.js
@@ -1,20 +1,24 @@
 import { Image, Text, TouchableOpacity, View } from 'react-native';
-import { styles } from './styles/GalleryListStyles';
+import { styles } from './styles/GalleryCardStyles';
 import { useMemo } from 'react';
 
 export default function GalleryCard({ item }) {
-  const height = useMemo(()=>Math.floor(Math.random()*100+200));
+  const height = useMemo(() => Math.floor(Math.random() * 100 + 200));
 
   return (
     <View>
       <TouchableOpacity activeOpacity={0.2}>
-        <Image source={{ uri: item.photos[0] }} style={[{
-          height:height,
-        },styles.itemImage]}
-        resizeMode='stretch' 
+        <Image
+          source={{ uri: item.photos[0] }}
+          style={[
+            {
+              height: height,
+            },
+            styles.itemImage,
+          ]}
+          resizeMode="stretch"
         />
       </TouchableOpacity>
     </View>
   );
 }
-
