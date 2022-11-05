624c2473c357be24987a63f81121c2301018b21c
Seojeonguk <junguk7880@naver.com>
2022-11-05 19:50:07 +0900

    Add input tag
    This will be able to type your email and password.
    
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

--- components/screen/Login.js
+++ components/screen/Login.js
@@ -1,5 +1,33 @@
+import { useState } from 'react';
+import { TextInput, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Login() {
-  return <SafeAreaView></SafeAreaView>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleChangeEmail = (newEmail) => {
+    setEmail(newEmail);
+  };
+
+  const handleChangePassword = (newPassword) => {
+    setPassword(newPassword);
+  };
+
+  return (
+    <SafeAreaView>
+      <View>
+        <TextInput
+          onChangeText={(newEmail) => handleChangeEmail(newEmail)}
+          placeholder="Email"
+          value={email}
+        />
+        <TextInput
+          onChangeText={(newPassword) => handleChangePassword(newPassword)}
+          placeholder="Password"
+          value={password}
+        />
+      </View>
+    </SafeAreaView>
+  );
 }
