4e9686ae2e2c9248aedf8091c8393481864c0fab
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2022-11-05 19:36:17 +0900

    Provide login navigation (#47)
    The start main page will be the login screen
    
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

--- components/navigation/Index.js
+++ components/navigation/Index.js
@@ -1,39 +1,18 @@
-import GalleryNavigation from './GalleryNavigation';
-import SettingsNavigation from './SettingsNavigation';
+import BottomNavigation from './BottomNavigation';
+import Login from '../screen/Login';
 
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
-import { FontAwesome, Entypo } from '@expo/vector-icons';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 import { NavigationContainer } from '@react-navigation/native';
 
-const Tab = createMaterialBottomTabNavigator();
+const Tab = createNativeStackNavigator();
 
 export default function Index() {
   return (
     <NavigationContainer>
-      <Tab.Navigator
-        initialRouteName="Gallery"
-        activeColor="#fdfffd"
-        inactiveColor="#a3c2ff"
-        shifting
-      >
-        <Tab.Screen
-          name="Gallery"
-          component={GalleryNavigation}
-          options={{
-            tabBarLabel: 'Gallery',
-            tabBarIcon: ({ color }) => <Entypo name="drink" size={24} color={color} />,
-            tabBarColor: '#eeb7b4',
-          }}
-        />
-        <Tab.Screen
-          name="Settings"
-          component={SettingsNavigation}
-          options={{
-            tabBarLabel: 'Settings',
-            tabBarIcon: ({ color }) => <FontAwesome name="gear" size={24} color={color} />,
-            tabBarColor: '#f2cfa5',
-          }}
-        />
+      <Tab.Navigator initialRouteName="login" screenOptions={{ headerShown: false }}>
+        <Tab.Screen name="login" component={Login} />
+        <Tab.Screen name="bottom" component={BottomNavigation} />
       </Tab.Navigator>
     </NavigationContainer>
   );
---
+++ components/screen/Login.js
@@ -0,0 +1,5 @@
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+export default function Login() {
+  return <SafeAreaView></SafeAreaView>;
+}
