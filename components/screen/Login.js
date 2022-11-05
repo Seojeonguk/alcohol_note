d2c2da6a22be16f3eb2cc3d066dadee8eb0c54d9
Seojeonguk <junguk7880@naver.com>
2022-11-05 19:30:25 +0900

    Add return
    This will return login component.
    
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

--- components/screen/Login.js
+++ components/screen/Login.js
@@ -1,5 +1,5 @@
-import { SafeAreaView } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Login() {
-  <SafeAreaView></SafeAreaView>;
+  return <SafeAreaView></SafeAreaView>;
 }
