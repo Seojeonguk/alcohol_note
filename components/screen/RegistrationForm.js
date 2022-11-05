b4e3a91dc28f5b5754ee80c306bf68a845fffeab
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2022-11-05 21:00:30 +0900

    Add inputs to the registration screen (#53)
    This will handle the input value on the registration screen.
    
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

--- components/screen/RegistrationForm.js
+++ components/screen/RegistrationForm.js
@@ -1,5 +1,55 @@
+import { useState } from 'react';
+import { StyleSheet, TextInput, View } from 'react-native';
+
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function RegistrationForm() {
-  return <SafeAreaView></SafeAreaView>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleChangeEmail = (newEmail) => {
+    setEmail(newEmail);
+  };
+
+  const handleChangePassword = (newPassword) => {
+    setPassword(newPassword);
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.inputBox}>
+        <TextInput
+          onChangeText={(newEmail) => handleChangeEmail(newEmail)}
+          placeholder="이메일을 입력해 주세요"
+          style={styles.input}
+          value={email}
+        />
+        <TextInput
+          onChangeText={(newPassword) => handleChangePassword(newPassword)}
+          placeholder="비밀번호를 입력해 주세요"
+          style={styles.input}
+          value={password}
+        />
+      </View>
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  input: {
+    backgroundColor: '#eeeeee',
+    borderColor: 'grey',
+    borderRadius: 5,
+    borderWidth: 1,
+    height: 40,
+    marginVertical: 10,
+    paddingHorizontal: 10,
+  },
+  inputBox: {
+    justifyContent: 'center',
+    padding: 20,
+  },
+});
