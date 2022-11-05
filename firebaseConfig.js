dd67a8cfc6d6856466fa9164ed1278bcb7a35a6a
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2022-11-05 21:41:35 +0900

    Config firebase (#58)
    This will config firebase.
    
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ firebaseConfig.js
@@ -0,0 +1,26 @@
+import { initializeApp } from 'firebase/app';
+
+// Optionally import the services that you want to use
+import { getAuth } from 'firebase/auth';
+//import {...} from "firebase/database";
+//import {...} from "firebase/firestore";
+//import {...} from "firebase/functions";
+//import {...} from "firebase/storage";
+
+// Initialize Firebase
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+// For more information on how to access Firebase in your project,
+// see the Firebase documentation: https://firebase.google.com/docs/web/setup#access-firebase
+
+export const auth = getAuth(app);
