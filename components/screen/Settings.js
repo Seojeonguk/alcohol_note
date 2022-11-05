674f6e22540ea0eeb7d224a8e70ec72aac5c6678
Seojeonguk <junguk7880@naver.com>
2022-11-05 17:52:14 +0900

    Add header
    This will show the header at the top of the settings screen.
    
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

--- components/screen/Settings.js
+++ components/screen/Settings.js
@@ -1,5 +1,32 @@
+import { StyleSheet, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Settings() {
-  return <SafeAreaView></SafeAreaView>;
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.settingsHeader}>
+        <Text style={styles.settingsTitle}>Settings</Text>
+      </View>
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  settingsHeader: {
+    alignItems: 'center',
+    borderBottomWidth: 1,
+    borderColor: 'grey',
+    borderStyle: 'solid',
+    justifyContent: 'center',
+    marginBottom: 5,
+    paddingVertical: 5,
+  },
+  settingsTitle: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
