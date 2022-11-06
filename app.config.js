bdd769a012c3ef0dc1ab15218de772fb59cce7d2
Seojeonguk <junguk7880@naver.com>
2022-11-06 18:15:19 +0900

    Change app config
    This will change from json to js to import dotenv.
    
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ app.config.js
@@ -0,0 +1,47 @@
+import 'dotenv/config';
+
+export default {
+  expo: {
+    name: 'alcoholic',
+    slug: 'alcoholic',
+    version: '1.0.0',
+    orientation: 'portrait',
+    icon: './assets/icon.png',
+    userInterfaceStyle: 'light',
+    splash: {
+      image: './assets/splash.png',
+      resizeMode: 'contain',
+      backgroundColor: '#ffffff',
+    },
+    updates: {
+      fallbackToCacheTimeout: 0,
+    },
+    assetBundlePatterns: ['**/*'],
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: 'com.seojeonguk.alcoholic',
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: './assets/adaptive-icon.png',
+        backgroundColor: '#FFFFFF',
+      },
+      package: 'com.seojeonguk.alcoholic',
+    },
+    web: {
+      favicon: './assets/favicon.png',
+    },
+    extra: {
+      eas: {
+        projectId: '090110c1-78a6-496f-9564-f32647afce83',
+      },
+      firebaseApiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+      firebaseAuthDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+      firebaseProjectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+      firebaseStorageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+      firebaseMessagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+      firebaseAppId: process.env.REACT_APP_FIREBASE_APP_ID,
+      firebaseMeasurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+    },
+  },
+};
