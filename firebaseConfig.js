d954394c0085a975b8f2ab8527d627c9b95b1f46
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2022-11-06 18:25:43 +0900

    Troubleshooting Errors Using .env Files (#62)
    This will give a value of .env, using the extras property.
    
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,4 +1,5 @@
 import { initializeApp } from 'firebase/app';
+import Constants from 'expo-constants';
 
 // Optionally import the services that you want to use
 import { getAuth } from 'firebase/auth';
@@ -10,13 +11,13 @@ import { getAuth } from 'firebase/auth';
 // Initialize Firebase
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  apiKey: Constants.manifest.extra.firebaseApiKey,
+  authDomain: Constants.manifest.extra.firebaseAuthDomain,
+  projectId: Constants.manifest.extra.firebaseProjectId,
+  storageBucket: Constants.manifest.extra.firebaseStorageBucket,
+  messagingSenderId: Constants.manifest.extra.firebaseMessagingSenderId,
+  appId: Constants.manifest.extra.firebaseAppId,
+  measurementId: Constants.manifest.extra.firebaseMeasurementId,
 };
 
 const app = initializeApp(firebaseConfig);
