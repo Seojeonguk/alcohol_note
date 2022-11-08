566765609390a2c6a99d4980771748c922693209
Seojeonguk <junguk7880@naver.com>
2022-11-08 20:33:22 +0900

    Handling unnecessary import annotations
    This will comment on unnecessary imports.
    
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -4,7 +4,7 @@ import { initializeApp } from 'firebase/app';
 import Constants from 'expo-constants';
 
 // Optionally import the services that you want to use
-import { getAuth } from 'firebase/auth';
+//import {...} from 'firebase/auth';
 //import {...} from "firebase/database";
 //import {...} from "firebase/firestore";
 //import {...} from "firebase/functions";
