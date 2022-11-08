bc3c99283c9f43210889349e8d7e450a4c97ec86
Seojeonguk <junguk7880@naver.com>
2022-11-08 22:51:31 +0900

    Add error message
    This will add errors related to login.
    
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

--- components/util/AuthError.js
+++ components/util/AuthError.js
@@ -4,6 +4,8 @@ const errorMap = {
   [AuthErrorCodes.EMAIL_EXISTS]: '이미 사용 중인 아이디입니다.',
   [AuthErrorCodes.INVALID_EMAIL]: '이메일 형식에 맞지 않는 메일 주소입니다.',
   [AuthErrorCodes.WEAK_PASSWORD]: '비밀번호는 최소 6자 이상이어야 합니다.',
+  [AuthErrorCodes.USER_DELETED]: '등록되지 않은 아이디입니다.',
+  [AuthErrorCodes.INVALID_PASSWORD]: '아이디 또는 비밀번호를 잘못 입력했습니다.',
   'missing/email': '이메일을 입력해 주세요.',
   'missing/password': '비밀번호를 입력해 주세요.',
 };
