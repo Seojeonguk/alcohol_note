95ca892b21cc162891f8fd1315c4243ee4e9ef74
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2022-12-21 19:26:05 +0900

    Let's design the settings screen (#45)
    This will provide a settings screen.
    
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

--- components/screen/Settings.js
+++ components/screen/Settings.js
@@ -1,5 +1,90 @@
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Settings() {
-  return <SafeAreaView></SafeAreaView>;
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.settingsHeader}>
+        <Text style={styles.settingsTitle}>Settings</Text>
+      </View>
+      <View style={styles.settingsBody}>
+        <View style={styles.userInfo}>
+          <Image
+            source={{
+              uri: 'https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMjA5MDZfMjUx%2FMDAxNjYyNDM3ODk4NTQ3.FDaxfffFN1iNSBH_t8yTmgw3l30r-1OZIBSyhESQLtYg.wvXYUDZtmsdL1of59htugexzGLe9tBd5VLuqjZqQ9mog.JPEG.janet_hwang%2FIMG_1365.jpg&type=a340',
+            }}
+            style={styles.userImg}
+          />
+          <Text style={styles.userNickname}>닉네임</Text>
+          <TouchableOpacity style={styles.userUpdateBtn}>
+            <Text style={styles.userUpdateBtnText}>수정</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.menus}>
+          <TouchableOpacity style={styles.menuItem}>
+            <Text style={styles.menuItemText}>공지사항</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.menuItem}>
+            <Text style={styles.menuItemText}>버전 정보</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.menuItem}>
+            <Text style={styles.menuItemText}>로그아웃</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.menuItem}>
+            <Text style={styles.menuItemText}>탈퇴하기</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  menuItem: {
+    borderBottomWidth: 1,
+    borderColor: '#888888',
+    marginHorizontal: 15,
+    paddingVertical: 5,
+  },
+  menuItemText: {
+    fontSize: 20,
+  },
+  menus: {
+    borderTopWidth: 1,
+    borderColor: '#888888',
+  },
+  settingsHeader: {
+    alignItems: 'center',
+    borderBottomWidth: 1,
+    borderColor: 'grey',
+    borderStyle: 'solid',
+    justifyContent: 'center',
+    marginBottom: 5,
+    paddingVertical: 5,
+  },
+  settingsTitle: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  userImg: {
+    height: 100,
+    width: 100,
+    borderRadius: 100,
+    marginBottom: 10,
+  },
+  userInfo: {
+    padding: 5,
+    alignItems: 'center',
+  },
+  userUpdateBtn: {
+    paddingHorizontal: 20,
+  },
+  userUpdateBtnText: {
+    color: '#50A4C5',
+  },
+});
