656fe82ca957c86f4f5f3bd98e01252cfaa89f22
Seojeonguk <junguk7880@naver.com>
2022-12-21 20:36:06 +0900

    Add main page
    This will add main page.
    
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

--- components/navigation/NoBottomNavigation.js
+++ components/navigation/NoBottomNavigation.js
@@ -1,4 +1,5 @@
 import Login from '../screen/Login';
+import MainPage from '../screen/MainPage';
 import RegistrationForm from '../screen/RegistrationForm';
 
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -7,7 +8,8 @@ const Tab = createNativeStackNavigator();
 
 export default function NoBottomNavigation() {
   return (
-    <Tab.Navigator initialRouteName="login" screenOptions={{ headerShown: false }}>
+    <Tab.Navigator initialRouteName="mainPage" screenOptions={{ headerShown: false }}>
+      <Tab.Screen name="mainPage" component={MainPage} />
       <Tab.Screen name="login" component={Login} />
       <Tab.Screen name="registration" component={RegistrationForm} />
     </Tab.Navigator>
---
+++ components/screen/MainPage.js
@@ -0,0 +1,67 @@
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+export default function MainPage() {
+  return (
+    <SafeAreaView>
+      <View style={styles.container}>
+        <View style={styles.mainImgWrap}>
+          <Image
+            source={{
+              uri: 'https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMjA5MDZfMjUx%2FMDAxNjYyNDM3ODk4NTQ3.FDaxfffFN1iNSBH_t8yTmgw3l30r-1OZIBSyhESQLtYg.wvXYUDZtmsdL1of59htugexzGLe9tBd5VLuqjZqQ9mog.JPEG.janet_hwang%2FIMG_1365.jpg&type=a340',
+            }}
+            style={styles.mainImg}
+          />
+        </View>
+
+        <View style={styles.btnWrap}>
+          <TouchableOpacity style={styles.loginBtnWrap}>
+            <Text style={styles.btnText}>로그인</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.registrationBtnWrap}>
+            <Text style={styles.btnText}>회원가입</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  btnWrap: {
+    alignItems: 'center',
+    flex: 1,
+  },
+  loginBtnWrap: {
+    borderRadius: 5,
+    height: 40,
+    width: 250,
+    backgroundColor: '#ABCEEA',
+    marginVertical: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  registrationBtnWrap: {
+    borderRadius: 5,
+    height: 40,
+    width: 250,
+    backgroundColor: '#A4AFC3',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  container: {
+    backgroundColor: '#F6E8DB',
+    minHeight: '100%',
+  },
+  mainImg: {
+    height: 200,
+    width: 200,
+    borderRadius: 100,
+  },
+  mainImgWrap: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 2,
+  },
+});
