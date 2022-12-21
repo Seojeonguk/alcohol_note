a7d8c67a7c6a304540bdb31640915045faf491b8
Seojeonguk <junguk7880@naver.com>
2022-12-21 19:20:12 +0900

    Show user information
    This will show user information.
    
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

--- components/screen/Settings.js
+++ components/screen/Settings.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Settings() {
@@ -6,6 +6,20 @@ export default function Settings() {
     <SafeAreaView style={styles.container}>
       <View style={styles.settingsHeader}>
         <Text style={styles.settingsTitle}>Settings</Text>
+      </View>
+      <View style={styles.settingsBody}>
+        <View style={styles.userInfo}>
+          <Image
+            source={{
+              uri: 'https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMjA5MDZfMjUx%2FMDAxNjYyNDM3ODk4NTQ3.FDaxfffFN1iNSBH_t8yTmgw3l30r-1OZIBSyhESQLtYg.wvXYUDZtmsdL1of59htugexzGLe9tBd5VLuqjZqQ9mog.JPEG.janet_hwang%2FIMG_1365.jpg&type=a340',
+            }}
+            style={styles.userImg}
+          />
+          <Text style={styles.userNickname}>닉네임</Text>
+          <TouchableOpacity style={styles.userUpdateBtn}>
+            <Text style={styles.userUpdateBtnText}>수정</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </SafeAreaView>
   );
@@ -28,5 +42,21 @@ const styles = StyleSheet.create({
     fontSize: 28,
     fontWeight: 'bold',
     textAlign: 'center',
+  },
+  userImg: {
+    height: 100,
+    width: 100,
+    borderRadius: 100,
+    marginBottom: 10,
+  },
+  userInfo: {
+    padding: 5,
+    alignItems: 'center',
+  },
+  userUpdateBtn: {
+    paddingHorizontal: 20,
+  },
+  userUpdateBtnText: {
+    color: '#50A4C5',
   },
 });
