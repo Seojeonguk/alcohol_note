2644f39f775ad2f2b1df35442c64dda7460eb5b8
Seojeonguk <junguk7880@naver.com>
2023-02-15 22:26:51 +0900

    Sign in after successful mail authentication
    This will proceed to the next page after the email authentication is completed.
    
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

--- components/navigation/Index.js
+++ components/navigation/Index.js
@@ -6,19 +6,79 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
 
 import { auth } from '../../firebaseConfig';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+
+import { signOut } from '@firebase/auth';
+
+import { sendEmailVerification } from '@firebase/auth';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import Toast from 'react-native-toast-message';
+import { Alert, TouchableOpacity } from 'react-native';
 
 const Tab = createNativeStackNavigator();
+const localStorageKeyName = 'confirmEmailExpireTime';
 
 export default function Index() {
   const [isLogin, setLogin] = useState(false);
-  auth.onAuthStateChanged((user) => {
-    if (user !== null) {
-      setLogin(true);
-    } else {
-      setLogin(false);
-    }
-  });
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(async (user) => {
+      console.log(user);
+      if (user) {
+        const confirmEmailExpireTime = await AsyncStorage.getItem(localStorageKeyName);
+        if (user.emailVerified) {
+          if (confirmEmailExpireTime) {
+            await AsyncStorage.removeItem(localStorageKeyName);
+          }
+          setLogin(true);
+        } else {
+          if (!confirmEmailExpireTime) {
+            await sendEmailVerification(user);
+            Toast.show({
+              type: 'success',
+              text1: '인증 메일을 발송하였습니다.',
+              text2: '인증 메일 확인 후 이용할 수 있습니다.',
+            });
+            // toast로 이메일 보냈다고 알려주기
+
+            const now = new Date();
+            now.setMinutes(now.getMinutes() + 30);
+
+            const obj = {
+              value: '1',
+              expire: now,
+            };
+
+            const objStr = JSON.stringify(obj);
+
+            await AsyncStorage.setItem(localStorageKeyName, objStr);
+          } else {
+            const obj = JSON.parse(confirmEmailExpireTime);
+            const t = new Date(obj.expire);
+            if (Date.now() > t) {
+              Alert.alert(
+                '재인증',
+                '인증 메일을 다시 보내시겠습니까?',
+                [
+                  { text: 'Cancel', style: 'cancel' },
+                  { text: 'Send', onPress: async () => await sendEmailVerification(user) },
+                ],
+                { cancelable: true }
+              );
+            } else {
+              Toast.show({
+                type: 'success',
+                text1: '인증 메일을 보냈으니 확인바랍니다.',
+                text2: `${t.getHours()}시 ${t.getMinutes()}분 ${t.getSeconds()}초 까지 메일을 다시 보낼 수 없습니다.`,
+              });
+            }
+          }
+          await signOut(auth);
+          setLogin(false);
+        }
+      } else setLogin(false);
+    });
+    return unsubscribe;
+  }, []);
   return (
     <NavigationContainer>
       <Tab.Navigator initialRouteName="nobottom" screenOptions={{ headerShown: false }}>
