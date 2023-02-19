2e8965b9726a982e88a148e4f55f1da1c17caf55
Seojeonguk <junguk7880@naver.com>
2023-02-19 14:07:40 +0900

    Modify toast text
    This will modify toast text.
    
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

--- components/navigation/Index.js
+++ components/navigation/Index.js
@@ -22,58 +22,59 @@ export default function Index() {
   const [isLogin, setLogin] = useState(false);
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged(async (user) => {
-      console.log(user);
-      if (user) {
-        const confirmEmailExpireTime = await AsyncStorage.getItem(localStorageKeyName);
-        if (user.emailVerified) {
-          if (confirmEmailExpireTime) {
-            await AsyncStorage.removeItem(localStorageKeyName);
-          }
-          setLogin(true);
+      if (!user) {
+        setLogin(false);
+        return;
+      }
+      const confirmEmailExpireTime = await AsyncStorage.getItem(localStorageKeyName);
+      if (user.emailVerified) {
+        if (confirmEmailExpireTime) {
+          await AsyncStorage.removeItem(localStorageKeyName);
+        }
+        setLogin(true);
+      } else {
+        if (!confirmEmailExpireTime) {
+          await sendEmailVerification(user);
+          Toast.show({
+            type: 'success',
+            text1: '인증 메일을 발송하였습니다.',
+            text2: '인증 메일 확인 후 이용할 수 있습니다.',
+          });
+          const now = new Date();
+          now.setMinutes(now.getMinutes() + 30);
+
+          const obj = {
+            value: '1',
+            expire: now,
+          };
+
+          const objStr = JSON.stringify(obj);
+
+          await AsyncStorage.setItem(localStorageKeyName, objStr);
         } else {
-          if (!confirmEmailExpireTime) {
-            await sendEmailVerification(user);
+          const obj = JSON.parse(confirmEmailExpireTime);
+          const t = new Date(obj.expire);
+          if (Date.now() > t) {
+            Alert.alert(
+              '재인증',
+              '인증 메일을 재전송 하시겠습니까?',
+              [
+                { text: 'Cancel', style: 'cancel' },
+                { text: 'Send', onPress: async () => await sendEmailVerification(user) },
+              ],
+              { cancelable: true }
+            );
+          } else {
             Toast.show({
               type: 'success',
-              text1: '인증 메일을 발송하였습니다.',
-              text2: '인증 메일 확인 후 이용할 수 있습니다.',
+              text1: '이메일의 메일함을 확인바랍니다.',
+              text2: `${t.getHours()}시 ${t.getMinutes()}분 ${t.getSeconds()}초 까지 메일을 다시 보낼 수 없습니다.`,
             });
-            const now = new Date();
-            now.setMinutes(now.getMinutes() + 30);
-
-            const obj = {
-              value: '1',
-              expire: now,
-            };
-
-            const objStr = JSON.stringify(obj);
-
-            await AsyncStorage.setItem(localStorageKeyName, objStr);
-          } else {
-            const obj = JSON.parse(confirmEmailExpireTime);
-            const t = new Date(obj.expire);
-            if (Date.now() > t) {
-              Alert.alert(
-                '재인증',
-                '인증 메일을 재전송 하시겠습니까?',
-                [
-                  { text: 'Cancel', style: 'cancel' },
-                  { text: 'Send', onPress: async () => await sendEmailVerification(user) },
-                ],
-                { cancelable: true }
-              );
-            } else {
-              Toast.show({
-                type: 'success',
-                text1: '인증 메일을 전송하였습니다.',
-                text2: `${t.getHours()}시 ${t.getMinutes()}분 ${t.getSeconds()}초 까지 메일을 다시 보낼 수 없습니다.`,
-              });
-            }
           }
-          await signOut(auth);
-          setLogin(false);
         }
-      } else setLogin(false);
+        await signOut(auth);
+        setLogin(false);
+      }
     });
     return unsubscribe;
   }, []);
