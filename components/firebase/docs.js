4e549dda7037a51c90c42507dfb141a86e0dd5f5
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2023-04-30 16:17:23 +0900

    Create new post (#130)
    This will create new post.
    
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

--- components/firebase/docs.js
+++ components/firebase/docs.js
@@ -1,13 +1,24 @@
-import { collection, doc, setDoc } from 'firebase/firestore';
+import { arrayUnion, collection, doc, setDoc, updateDoc } from 'firebase/firestore';
 import { db } from '../../firebaseConfig';
 
 const usersRef = collection(db, 'users');
+const postsRef = collection(db, 'posts');
 
 const createUserInfo = async (email) => {
-  await setDoc(doc(usersRef, email), {
+  const newUserDoc = doc(usersRef, email);
+  await setDoc(newUserDoc, {
     userEmail: email,
     posts: [],
   });
 };
 
-export { createUserInfo };
+const createNewPost = async (data, email) => {
+  const newPostDoc = doc(postsRef);
+  await setDoc(newPostDoc, data);
+
+  await updateDoc(doc(usersRef, email), {
+    posts: arrayUnion(newPostDoc.id),
+  });
+};
+
+export { createUserInfo, createNewPost };
