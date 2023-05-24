c46c4b7469393c254710c7f19bf45e7c9a4e7bf6
Seojeonguk <junguk7880@naver.com>
2023-05-24 18:41:05 +0900

    Remove variables
    This will remove unnecessary variable.
    
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

--- screens/ForgotPassword.js
+++ screens/ForgotPassword.js
@@ -11,7 +11,6 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function ForgetPassword({ navigation }) {
   const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
   const [emailError, setEmailError] = useState('');
   const emailRef = useRef();
 
