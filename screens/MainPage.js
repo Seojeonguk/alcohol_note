1dc8d2e190a52c2f099281c24caaaec90845173b
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2023-06-01 23:04:05 +0900

    Change main page style (#183)
    This will change main page style.
    
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

--- screens/MainPage.js
+++ screens/MainPage.js
@@ -35,21 +35,25 @@ export default function MainPage({ navigation, route }) {
   }, []);
 
   return (
-    <SafeAreaView>
-      <View style={styles.container}>
-        <View style={styles.mainImgWrap}>
-          <Image source={mainPageImg} style={styles.mainImg} />
-        </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.mainImgWrap}>
+        <Image source={mainPageImg} style={styles.mainImg} />
+      </View>
 
-        <View style={styles.btnWrap}>
-          <TouchableOpacity onPress={handleLoginBtn} style={styles.loginBtnWrap}>
-            <Text style={styles.btnText}>로그인</Text>
-          </TouchableOpacity>
+      <View style={styles.btnWrap}>
+        <TouchableOpacity
+          onPress={handleLoginBtn}
+          style={[styles.btn, { backgroundColor: COLORS.BUTTON_COLOR }]}
+        >
+          <Text style={styles.btnText}>로그인</Text>
+        </TouchableOpacity>
 
-          <TouchableOpacity onPress={handleRegistrationBtn} style={styles.registrationBtnWrap}>
-            <Text style={styles.btnText}>회원가입</Text>
-          </TouchableOpacity>
-        </View>
+        <TouchableOpacity
+          onPress={handleRegistrationBtn}
+          style={[styles.btn, { backgroundColor: COLORS.THIRD_SUB_COLOR }]}
+        >
+          <Text style={styles.btnText}>회원가입</Text>
+        </TouchableOpacity>
       </View>
     </SafeAreaView>
   );
@@ -60,26 +64,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     flex: 1,
   },
-  loginBtnWrap: {
+  btn: {
     alignItems: 'center',
-    backgroundColor: COLORS.BUTTON_COLOR,
     borderRadius: 5,
     height: 40,
     justifyContent: 'center',
     marginVertical: 10,
     width: 250,
   },
-  registrationBtnWrap: {
-    alignItems: 'center',
-    backgroundColor: '#A4AFC3',
-    borderRadius: 5,
-    height: 40,
-    justifyContent: 'center',
-    width: 250,
-  },
   container: {
     backgroundColor: COLORS.BACKGROUND_COLOR,
-    minHeight: '100%',
+    flex: 1,
   },
   mainImg: {
     borderRadius: 100,
