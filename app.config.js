8f8000b4a77745020eef15b69075a76747882cfb
Seojeonguk <junguk7880@naver.com>
2023-10-19 18:24:29 +0900

    Fix untracked eas configuration file
    This will untrack the eas configuration file.
    
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

--- app.config.js
+++ app.config.js
@@ -35,13 +35,13 @@ export default {
       eas: {
         projectId: '090110c1-78a6-496f-9564-f32647afce83',
       },
-      firebaseApiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-      firebaseAuthDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-      firebaseProjectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-      firebaseStorageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-      firebaseMessagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-      firebaseAppId: process.env.REACT_APP_FIREBASE_APP_ID,
-      firebaseMeasurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+      firebaseApiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+      firebaseAuthDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+      firebaseProjectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+      firebaseStorageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+      firebaseMessagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+      firebaseAppId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+      firebaseMeasurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
     },
     plugins: [
       [
