1a6d5c9a2c423750e71b53d0d0e4e711f8818453
Seojeonguk <junguk7880@naver.com>
2023-10-20 21:53:06 +0900

    Prevent to re-rendering
    This will prevent to re-rendering.
    
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

--- components/Carousel.js
+++ components/Carousel.js
@@ -1,40 +1,42 @@
-import React, { useState } from 'react';
+import React, { memo, useState } from 'react';
 import { Dimensions, FlatList, Image, View } from 'react-native';
 import Indicator from './Indicator';
 
-export default function Carousel({ height, data, offset, gap, isIndicator }) {
+const RenderItem = memo(({ item, gap, width }) => {
+  return (
+    <View style={{ width: width, marginHorizontal: gap / 2 }}>
+      <Image
+        source={{ uri: item }}
+        style={{ height: '100%', resizeMode: 'stretch', marginHorizontal: gap / 2 }}
+      />
+    </View>
+  );
+});
+
+const ListEmptyComponent = ({ width }) => {
+  const DEFAULT_PHOTO_URI =
+    'https://firebasestorage.googleapis.com/v0/b/alcoholic-a9f86.appspot.com/o/default.jfif?alt=media&token=18e253aa-6a28-4a68-9823-eaf0726b6830';
+  return (
+    <View style={{ width: width }}>
+      <Image
+        source={{ uri: DEFAULT_PHOTO_URI }}
+        style={{ height: '100%', resizeMode: 'stretch' }}
+      />
+    </View>
+  );
+};
+
+function Carousel({ height, data, offset, gap, isIndicator }) {
   const [index, setIndex] = useState(0);
 
   const screenWidth = Dimensions.get('window').width;
   const width = screenWidth - (gap + offset) * 2;
 
-  const renderItem = ({ item }) => {
-    return (
-      <View style={{ width: width, marginHorizontal: gap / 2 }}>
-        <Image
-          source={{ uri: item }}
-          style={{ height: '100%', resizeMode: 'stretch', marginHorizontal: gap / 2 }}
-        />
-      </View>
-    );
-  };
-
-  const ListEmptyComponent = () => {
-    const DEFAULT_PHOTO_URI =
-      'https://firebasestorage.googleapis.com/v0/b/alcoholic-a9f86.appspot.com/o/default.jfif?alt=media&token=18e253aa-6a28-4a68-9823-eaf0726b6830';
-    return (
-      <View style={{ width: width }}>
-        <Image
-          source={{ uri: DEFAULT_PHOTO_URI }}
-          style={{ height: '100%', resizeMode: 'stretch' }}
-        />
-      </View>
-    );
-  };
-
   const scrollEnd = (e) => {
     const changedIndex = Math.round(e.nativeEvent.contentOffset.x / (width + gap));
-    setIndex(changedIndex);
+    if (index != changedIndex) {
+      setIndex(changedIndex);
+    }
   };
 
   return (
@@ -54,13 +56,15 @@ export default function Carousel({ height, data, offset, gap, isIndicator }) {
         keyExtractor={(item) => `page__${item}`}
         onMomentumScrollEnd={scrollEnd}
         pagingEnabled
-        renderItem={renderItem}
+        renderItem={({ item }) => <RenderItem item={item} gap={gap} width={width} />}
         snapToInterval={width + gap}
         snapToAlignment="start"
         showsHorizontalScrollIndicator={false}
-        ListEmptyComponent={ListEmptyComponent}
+        ListEmptyComponent={() => <ListEmptyComponent width={width} />}
       />
       {isIndicator && <Indicator length={data.length || 1} index={index} />}
     </View>
   );
 }
+
+export default memo(Carousel);
